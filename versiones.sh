926ccf25821d49af755dba949da2cbd4a584d7d0
Jose Texier <dantexier@yahoo.com>
2021-08-26 23:05:45 -0300

    Cambios de prueba

---
+++ versiones.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "- nvm: nvm --version"
+nvm --version
+echo "- nvm use: nvm use v"
+nvm use v
+
+echo "- npm:"
+npm -v
+echo "- node:"
+node -v
+echo "- nodejs:"
+nodejs --version
+echo "- Ruby:"
+ruby -v
+echo "- Bower:"
+bower -v
+echo "- Sass:"
+sass -v
+echo "- Compass:"
+compass -v
+echo "- Grunt:"
+grunt --version
+
